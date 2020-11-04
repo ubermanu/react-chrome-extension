1a85e90e4448d60b4f7968cfc5911cb9cd28f64a
ubermanu <e.vodor@gmail.com>
2020-11-04 13:46:52 +0100

    Add custom webpack config

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Chrome extension example (React)
 
 This is an example of a GoogleChrome extension with injected React application.<br>
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Compact React](https://github.com/mblydenburgh/no-cra-template/).
 
 ## Build
 
 Build the project with node then attach the build dir in Chrome Extensions.<br>
 You must enable the Developer mode (top right button in the extensions menu).
 
-## Known issues
-
-Local resources (like images) are not loaded because of urls (to investigate)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'extension.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: ['css-loader', 'style-loader'] },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/,
+        use: 'base64-inline-loader?limit=1000&name=[name].[ext]',
+      },
+    ],
+  },
+  mode: 'development',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      inject: !!process.env.WEBPACK_DEV_SERVER,
+    }),
+  ],
+}
